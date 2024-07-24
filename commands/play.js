946857e5a9fb4c8f98f87d758f08cf015402c1b0
Leandro Novaes <leandrofern.novaes@gmail.com>
2024-07-24 21:24:45 +0100

    feat: create docker compose

--- commands/play.js
+++ commands/play.js
@@ -27,7 +27,7 @@ function createRowGroup(start, end) {
   let action = new ActionRowBuilder();
   for (let i = start; i <= end; i++) {
     let actionsLength = action.components.length;
-
+    console.log('actions length', actionsLength);
     if (actionsLength === 5) {
       rows.push(action);
       action = new ActionRowBuilder();
@@ -52,34 +52,6 @@ function createRowGroup(start, end) {
   return rows;
 }
 
-async function executeSubCommandMeme(interaction) {
-  const { commandName } = interaction;
-
-  if (commandName === 'play') {
-    try {
-      const subCommand = interaction?.options?.getSubcommand();
-
-      if (subCommand !== 'meme') return;
-
-      const meme = interaction.options.getString('meme');
-      
-      const sound = sounds.find(sound => sound.name === meme || sound.id === meme);
-
-      if (!sound) {
-        await interaction.reply({ content: 'meme nao encontrado!', ephemeral: true });
-        return;
-      }
-
-      await playMeme(sound.url, interaction);
-      await interaction.reply(`${interaction.user.username} invocou em ${sound.name}!`);
-      return true;
-    } catch (error) {
-      console.log('Is not a subcommand!');
-      return false;
-    }
-  }
-}
-
 async function execute({ interaction }) {
   const { voice } = interaction.member;
 
@@ -91,40 +63,18 @@ async function execute({ interaction }) {
   await interaction.reply({ content: 'Carregando os memes...', ephemeral: true });
   
   const totalRows = Math.ceil(sounds.length/25);
-
+  console.log('total rows', totalRows);
   for (let i = 0; i < totalRows; i++) {
     const start = i * 25;
     const end = start + 25;
 
     const rows = createRowGroup(start, end, ButtonStyle.Primary);
+    console.log('rows', rows.length);
     await interaction.followUp({ content: `Pagina ${i+1} de memes disponiveis:`, components: rows });
   }
 }
 
-// function shuffleSounds() {
-//   const shuffled = [...sounds];
-//   for (let i = shuffled.length - 1; i > 0; i--) {
-//       const j = Math.floor(Math.random() * (i + 1));
-//       [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-//   }
-
-//   return shuffled;
-// }
-
 async function interaction({ interaction }) {
-  // const { commandName } = interaction;
-
-  // if (interaction.type === InteractionType.ApplicationCommandAutocomplete && commandName === 'play') {
-  //   const focusedValue = interaction.options.getFocused();
-  //   const shuffledSounds = shuffleSounds();
-  //   const filtered = shuffledSounds.filter(sound => sound.name.toLowerCase().includes(focusedValue.toLowerCase()));
-  //   const sliced = filtered.slice(0, 24);
-
-  //   await interaction.respond(
-  //     sliced.map(sound => ({ name: `${sound.emoji || '😄'}  ${sound.name}` , value: sound.id }))
-  //   );
-  // }
-
   if (interaction.type === InteractionType.MessageComponent) {
     const from = interaction?.customId.split('_')[0];
   
