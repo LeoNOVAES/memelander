03f80ec41c64cdc38da4477a30893ce174f64f03
sousacaio <frias.caio@yahoo.com.br>
2024-08-01 10:48:41 +0100

    refactor: make play available and working

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -8,6 +8,7 @@ import {
 import { clearTimeoutBot, playMeme } from '../services/actionsService';
 import * as memeRepository from '../repository/memes.repository';
 import * as serverRepository from '../repository/server.repository';
+import { render as MemeButtonsComponent } from './shared-components/meme-buttons'
 
 export function body(): SlashCommandBuilder {
     try {
@@ -20,57 +21,9 @@ export function body(): SlashCommandBuilder {
     }
 }
 
-export async function sendRowGroupFollowUp(pages: number, interaction: any): Promise<void> {
-    console.log('creating group row');
-    const colors = [
-        { color: ButtonStyle.Primary },
-        { color: ButtonStyle.Secondary },
-        { color: ButtonStyle.Success },
-        { color: ButtonStyle.Danger },
-        { color: ButtonStyle.Primary },
-        { color: ButtonStyle.Success },
-    ];
-
-    for (let i = 0; i < pages; i++) {
-        const rows = [];
-        let action = new ActionRowBuilder();
-        const sounds = await memeRepository.findAllPaginated(i + 1);
-
-        if (!sounds || !sounds.length) return;
-
-        for (let j = 0; j < sounds.length; j++) {
-            let actionsLength = action.components.length;
-            const emoji = sounds[j]?.emoji || '🤣';
-
-            if (actionsLength === 5) {
-                rows.push(action);
-                action = new ActionRowBuilder();
-                actionsLength = action.components.length;
-            }
-
-            action.addComponents(
-                new ButtonBuilder()
-                    .setCustomId(sounds[j].memeId)
-                    .setLabel(`${emoji} ${sounds[j].name}`)
-                    .setStyle(colors[actionsLength]?.color)
-            );
-
-            if (!sounds[j + 1]?.name) {
-                rows.push(action);
-            }
-        }
-
-        await interaction.followUp({
-            content: `Pagina ${i + 1} de memes disponiveis:`,
-            components: [...rows],
-            ephemeral: true,
-        });
-    }
-}
-
 export async function execute(interaction: any): Promise<void> {
-    const { voice } = interaction.member;
     const server = await serverRepository.findById(interaction.guildId);
+    const { voice } = interaction.member;
 
     if (!voice.channel) {
         interaction.reply({
@@ -88,9 +41,13 @@ export async function execute(interaction: any): Promise<void> {
         servers: server._id || interaction.guildId,
     }
     const total = await memeRepository.count(query);
+    if (total === 0) {
+        await interaction.followUp({ content: 'Nenhum meme cadastrado! por favor use o comando /add e depois o botao adicionar memes!', ephemeral: true });
+        return;
+    }
     const totalPages = Math.ceil(total / 25);
     console.log('total totalPages', totalPages);
-    await sendRowGroupFollowUp(totalPages, interaction);
+    await MemeButtonsComponent(totalPages, interaction, query);
 }
 
 export async function interaction(interaction: any): Promise<void> {
